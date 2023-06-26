b865b949a88fcb728f34d94e09e49ddafce2f1e8
KOOBONSIK <megatron1234@naver.com>
2023-06-26 13:13:51 +0900

    # 5. 토큰 인증 처리

---
+++ src/main/java/com/example/todo/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.example.todo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.pattern.PathPattern;
+
+//전역 크로스 오리진 설정
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+            .addMapping("/api/**") //어떤 요청에 대해서 허용할지?
+            .allowedOrigins("http://localhost:3000") //어떤 클라이언트를 허용할지
+            .allowedMethods("*") //어떤 요청 방식을 허용할지
+            .allowedHeaders("*") //어떤 요청 헤더을 허용할지
+            .allowCredentials(true) //쿠키 전달을 허용할 것인지
+            .maxAge(3600) //캐싱 시간을 설정
+            ;
+  }
+}
--- src/main/java/com/example/todo/config/EnableWebSecurity.java
+++
@@ -1,4 +0,0 @@
-package com.example.todo.config;
-
-public @interface EnableWebSecurity {
-}
